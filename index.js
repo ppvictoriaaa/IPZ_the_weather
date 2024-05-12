edcaaf1b21a566ad108381144423312ab6ae31b3
ppvictoriaaa <pekurvika@gmail.com>
2024-05-12 19:19:03 +0300

    add mysql connection and change some files

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const express = require("express");
+const mysql = require("mysql");
 const path = require("path");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.env" });
 
 const app = express();
 
-// Вказуємо Express використовувати статичні файли з папки FRONTEND
-app.use(express.static(path.join(__dirname, "FRONTEND")));
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+});
 
-// Обробляємо запит на кореневий шлях і віддаємо index.html
+app.set("view engine", "hbs");
+
+db.connect((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("MySql Connected...");
+  }
+});
+
+const publicDirectory = path.join(__dirname, "./public");
+app.use(express.static(publicDirectory));
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "FRONTEND", "index.html"));
+  res.render("index");
 });
 
 const PORT = 3000;
