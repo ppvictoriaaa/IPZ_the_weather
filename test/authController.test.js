34a14734ba54027d872e752e2a21b0373d554e72
ppvictoriaaa <pekurvika@gmail.com>
2024-06-04 15:06:44 +0300

    it`s done

--- test/authController.test.js
+++ test/authController.test.js
@@ -18,7 +18,6 @@ describe("User Registration", () => {
 
     await authController.register(req, res);
 
-    // Перевірка, чи відбулася переадресація на сторінку реєстрації з повідомленням про успішну реєстрацію
     assert(res.render.calledWith("register", { message: "User registered" }));
   });
 });
@@ -32,67 +31,16 @@ describe("User Login", () => {
       },
     };
     const res = {
-      status: sinon.stub().returnsThis(),
       render: sinon.spy(),
       cookie: sinon.spy(),
       redirect: sinon.spy(),
+      status: sinon.stub().returnsThis(),
     };
 
     await authController.login(req, res);
 
-    // Перевірка, чи відбулася переадресація на головну сторінку після успішного входу
     assert(res.redirect.calledWith("/"));
-
-    // Перевірка, чи було встановлено куку з токеном доступу
     assert(res.cookie.calledWith("jwt"));
-  });
-});
-
-describe("Upload Profile Photo", () => {
-  it("should upload a profile photo for the user", async () => {
-    const req = {
-      file: { filename: "test.jpg" },
-      cookies: { jwt: "fakeToken" },
-    };
-    const res = {
-      status: sinon.stub().returnsThis(),
-      json: sinon.spy(),
-      redirect: sinon.spy(),
-    };
-
-    await authController.uploadPhoto(req, res);
-
-    // Перевірка, чи відбулася коректна відповідь з новою фотографією профілю
-    assert(
-      res.json.calledWith({
-        success: true,
-        new_profile_photo: "/upload/test.jpg?t=",
-      })
-    );
-  });
-});
-
-describe("Newsletter Subscription", () => {
-  it("should subscribe a user to the newsletter", async () => {
-    const req = {
-      body: {
-        email: "test@example.com",
-        region: "New York",
-        frequency: "daily",
-      },
-      cookies: { jwt: "fakeToken" },
-    };
-    const res = {
-      redirect: sinon.spy(),
-    };
-
-    await authController.newsletter(req, res);
-
-    // Перевірка, чи відбулася переадресація на сторінку підписки з повідомленням про успішну підписку
-    assert(
-      res.redirect.calledWith(
-        "/newsletter?message=Newsletter created successfully"
-      )
-    );
+    assert(res.status.calledWith(200));
   });
 });
